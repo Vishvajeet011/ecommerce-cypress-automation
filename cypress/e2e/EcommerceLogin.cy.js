cc481fa4e04f7c852cf6cc9c5d44d77dd5ce9f8f
Vishvajeet <vishvajeetsingh065@gmail.com>
2025-12-10 10:16:05 +0530

    Testing CI/CD integration trigger

--- cypress/e2e/EcommerceLogin.cy.js
+++ cypress/e2e/EcommerceLogin.cy.js
@@ -1,6 +1,6 @@
 import Login from "../POM/Login"
 
-describe("Login test", ()=>{
+describe("Login test with CI/CD", ()=>{
     it("Login positive test", ()=>{
         cy.fixture('EcommerceLogin').then((data=>{
         const ln = new Login();
